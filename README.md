db10709b5e5ef3db21b83fc4fec6319b8c7cbc5b
Joel Yap <joelytk@gmail.com>
2024-08-28 01:10:30 +0800

    Completed freeCodeCamp challenge

---
+++ README.md
@@ -0,0 +1,5 @@
+# D3 bar chart
+
+- **Framework**: [D3.js](https://d3js.org/)
+- **Deployment**: [GitHub Pages](https://pages.github.com/)
+- **Styling**: CSS3
---
+++ script.js
@@ -0,0 +1,114 @@
+const projectName = 'bar-chart';
+
+const width = 800;
+const height = 400;
+
+const fetchData = async () => {
+  try {
+    const res = await fetch(
+      'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
+    );
+    const data = await res.json();
+    createBarChart(data);
+  } catch (error) {
+    console.error({ error });
+  }
+};
+
+const createBarChart = data => {
+  const { data: dataset, from_date, to_date } = data;
+  const barWidth = width / dataset.length;
+
+  const getQuarter = date =>
+    `Q${Math.floor((new Date(date).getMonth() + 3) / 3)}`;
+
+  const getYear = date => Number(date.split('-')[0]);
+
+  // Initialize the x and y scales
+  const xScale = d3.scaleTime(
+    [new Date(from_date), new Date(to_date)],
+    [0, width]
+  );
+  const yScale = d3.scaleLinear([0, d3.max(dataset, d => d[1])], [height, 0]);
+
+  // Initialize the x and y axes
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
+
+  // Create the SVG element
+  const svg = d3
+    .select('.bar-chart-container')
+    .append('svg')
+    .attr('width', width + 100)
+    .attr('height', height + 60);
+
+  // Create the tooltip
+  const tooltip = d3
+    .select('.bar-chart-container')
+    .append('div')
+    .attr('id', 'tooltip')
+    .style('opacity', 0);
+
+  // Add the axes and their labels
+  svg
+    .append('text')
+    .attr('id', 'x-axis-label')
+    .attr('x', 485)
+    .attr('y', 453)
+    .attr('fill', '#fff')
+    .attr('font-size', '0.8rem')
+    .attr('text-anchor', 'middle')
+    .text('Year');
+  svg
+    .append('text')
+    .attr('id', 'y-axis-label')
+    .attr('x', 20)
+    .attr('y', 230)
+    .attr('fill', '#fff')
+    .attr('font-size', '0.8rem')
+    .attr('text-anchor', 'middle')
+    .text('GDP');
+  svg
+    .append('g')
+    .attr('id', 'x-axis')
+    .attr('transform', `translate(80, 420)`)
+    .call(xAxis);
+  svg
+    .append('g')
+    .attr('id', 'y-axis')
+    .attr('transform', `translate(80, 20)`)
+    .call(yAxis);
+
+  // Create the bars, then add class and titles
+  svg
+    .selectAll('rect')
+    .data(dataset)
+    .enter()
+    .append('rect')
+    .attr('x', d => xScale(new Date(d[0])) + 80)
+    .attr('y', d => yScale(d[1]) + 20)
+    .attr('width', barWidth)
+    .attr('height', d => height - yScale(d[1]))
+    .attr('class', 'bar')
+    .attr('data-date', d => d[0])
+    .attr('data-gdp', d => d[1])
+    .on('mouseover', e => {
+      const [date, value] = e.target.__data__;
+
+      tooltip
+        .style('opacity', 1)
+        .style('top', `${window.event.pageY}px`)
+        .style('left', `${window.event.pageX}px`)
+        .style('transform', 'translate(-50%, -100%)')
+        .attr('data-date', date).html(`
+          <span>${getQuarter(date)} ${getYear(date)}</span>
+          <br />
+          <span>$${value.toLocaleString()} billion</span>
+        `);
+    })
+    .on('mouseout', () => {
+      tooltip.style('opacity', 0);
+    });
+};
+
+fetchData();
